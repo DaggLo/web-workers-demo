893c2da463ecd728153d1fa00d6d1cff1f0cfec2
Evgeny Degtev <e.a.degtev@gmail.com>
2016-02-19 00:43:26 +0300

    fix: Fix path to a worker.js.

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -6,7 +6,7 @@
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
   
-  var worker = new Worker('worker.js');
+  var worker = new Worker('scripts/worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
